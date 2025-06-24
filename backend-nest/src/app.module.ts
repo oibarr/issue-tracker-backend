5224aaa6733fede2c2efbd39270d9427e9ed14fe
Osvaldo Ibarra <oibarr@outlook.com>
2025-06-23 23:25:45 -0500

    implements users module

--- backend-nest/src/app.module.ts
+++ backend-nest/src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { User } from './users/user.entity';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'dev.sqlite',
+      entities: [User],
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
 })
 export class AppModule {}
---
+++ backend-nest/src/users/users.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepo: Repository<User>,
+  ) {}
+
+  async createUser(email: string, passwordHash: string): Promise<User> {
+    const user = this.usersRepo.create({ email, passwordHash });
+    return this.usersRepo.save(user);
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    return this.usersRepo.findOne({ where: { email } });
+  }
+
+  async findById(id: number): Promise<User | null> {
+    return this.usersRepo.findOne({ where: { id } });
+  }
+}
